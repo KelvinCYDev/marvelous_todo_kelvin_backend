a1ef916108f78792c00fcd3ae32a216d347a9cf5
kelvincyhk <kelvincyhk@gmail.com>
2023-07-28 22:17:53 -0700

    Closes #1.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
@@ -7,5 +8,13 @@ const PORT = process.env.PORT | 5000;
 
 app.use(express.json());
 app.use(cors());
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Mongodb Connected Successfully"))
+  .catch((err) => console.error(err));
 
 app.listen(PORT, () => console.log("Server running on port " + PORT));
